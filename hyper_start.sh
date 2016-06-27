ae7f313d98ed65a1b9ee8494c500a37693194386
Siriwat Kasamwattanarote <siriwat.k@sbibits.com>
2016-06-27 11:22:24 +0900

    Added support for specifying PBFT or NOOP consensus.

--- hyper_start.sh
+++ hyper_start.sh
@@ -10,7 +10,7 @@ total_node=$1
 
      if [ -z "$total_node" ]
           then
-          echo "Usage $0 total_node"
+          echo "Usage $0 total_node [node_prefix] [consensus_mode]"
           echo "Please specify the total number of node"
           exit
      fi     
@@ -28,6 +28,13 @@ if [ ! -z "$2" ]
      udl="_"
 fi
 
+# Consensus
+consensus=""
+if [ ! -z "$3" ]
+     then
+     consensus="$3"
+fi
+
 # Node range
 if [ $total_node -gt 0 ]
      then     
@@ -35,7 +42,7 @@ if [ $total_node -gt 0 ]
      while [ $idx -lt $total_node ]; do
           #ssh node01.bc.ssd.dev.fu screen -d -m -S HyperNode01 /hyperledger/compose/register_node.sh 0; screen -r Hyper
           printf "Starting.. node0$((idx+1)).bc.ssd.dev.fu:${prefix}${udl}vp${idx} .."
-          ssh node0$((idx+1))${domain} screen -d -m -S ${prefix}${udl}HyperNode0$((idx+1)) ${regist_cmd} ${idx} ${prefix}
+          ssh node0$((idx+1))${domain} screen -d -m -S ${prefix}${udl}HyperNode0$((idx+1)) ${regist_cmd} ${idx} ${prefix} ${consensus}
           echo "done!"
           ((idx++))
      done
@@ -44,6 +51,6 @@ if [ $total_node -gt 0 ]
 else
      total_node=$((total_node*-1))
      printf "Starting.. node0${total_node}.bc.ssd.dev.fu:${prefix}${udl}vp$((total_node-1)) .."
-     ssh node0${total_node}${domain} screen -d -m -S ${prefix}${udl}HyperNode0${total_node} ${regist_cmd} $((total_node-1)) ${prefix}
+     ssh node0${total_node}${domain} screen -d -m -S ${prefix}${udl}HyperNode0${total_node} ${regist_cmd} $((total_node-1)) ${prefix} ${consensus}
      echo "done!"
 fi
--- register_node.sh
+++ register_node.sh
@@ -25,68 +25,148 @@ if [ ! -z "$2" ]
      udl="_"
 fi
 
+# Consensus
+consensus=""
+if [ ! -z "$3" ]
+     then
+     consensus="$3"
+fi
+
 cd /hyperledger/compose/configuration
 
+# ROOT node
 if [ $node_no -eq 0 ]
      then
-     docker run --rm --name=${prefix}${udl}vp${node_no} \
-                    -it \
-                    --net=host \
-                    -p 5000:5000 \
-                    -p 30303:30303 \
-                    -p 30304:30304 \
-                    -p 31315:31315 \
-                    -v /var/run/docker.sock:/var/run/docker.sock \
-                    -v /hyperledger/db:/var/hyperledger/db \
-                    -v /hyperledger/production:/var/hyperledger/production \
-                    -v /hyperledger/chaincode:/var/hyperledger/chaincode \
-                    -v /hyperledger/chaincode:/go/src/chaincode \
-                    -v /hyperledger/compose:/var/hyperledger/compose \
-                    -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
-                    -e CORE_PEER_ID=vp${node_no} \
-                    -e CORE_PEER_ADDRESSAUTODETECT=true \
-                    -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
-                    -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
-                    -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=pbft \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
-                    -e CORE_PBFT_GENERAL_N=4 \
-                    -e CORE_PBFT_GENERAL_K=2 \
-                    -e CORE_PBFT_GENERAL_TIMEOUT_REQUEST=10s \
-                    -e CORE_PBFT_GENERAL_MODE=classic \
-                    -e CORE_SECURITY_ENABLED=false \
-                    -e CORE_LOGGING_LEVEL=${debug_level} \
-                    stylix/hyperledger-peer:pbft peer node start
+     if [ "$consensus" == "pbft" ]
+          then
+          # Consensus PBFT mode
+          echo -e "..NOOPS.."
+          docker run --rm --name=${prefix}${udl}vp${node_no} \
+                         -it \
+                         --net=host \
+                         -p 5000:5000 \
+                         -p 30303:30303 \
+                         -p 30304:30304 \
+                         -p 31315:31315 \
+                         -v /var/run/docker.sock:/var/run/docker.sock \
+                         -v /hyperledger/db:/var/hyperledger/db \
+                         -v /hyperledger/production:/var/hyperledger/production \
+                         -v /hyperledger/chaincode:/var/hyperledger/chaincode \
+                         -v /hyperledger/chaincode:/go/src/chaincode \
+                         -v /hyperledger/compose:/var/hyperledger/compose \
+                         -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
+                         -e CORE_PEER_ID=vp${node_no} \
+                         -e CORE_PEER_ADDRESSAUTODETECT=true \
+                         -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
+                         -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
+                         -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=pbft \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
+                         -e CORE_PBFT_GENERAL_N=4 \
+                         -e CORE_PBFT_GENERAL_K=2 \
+                         -e CORE_PBFT_GENERAL_TIMEOUT_REQUEST=10s \
+                         -e CORE_PBFT_GENERAL_MODE=classic \
+                         -e CORE_SECURITY_ENABLED=false \
+                         -e CORE_LOGGING_LEVEL=${debug_level} \
+                         stylix/hyperledger-peer:pbft peer node start
+     else
+          # Consensus NOOPS mode
+          echo -e "..NOOPS.."
+          docker run --rm --name=${prefix}${udl}vp${node_no} \
+                         -it \
+                         --net=host \
+                         -p 5000:5000 \
+                         -p 30303:30303 \
+                         -p 30304:30304 \
+                         -p 31315:31315 \
+                         -v /var/run/docker.sock:/var/run/docker.sock \
+                         -v /hyperledger/db:/var/hyperledger/db \
+                         -v /hyperledger/production:/var/hyperledger/production \
+                         -v /hyperledger/chaincode:/var/hyperledger/chaincode \
+                         -v /hyperledger/chaincode:/go/src/chaincode \
+                         -v /hyperledger/compose:/var/hyperledger/compose \
+                         -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
+                         -e CORE_PEER_ID=vp${node_no} \
+                         -e CORE_PEER_ADDRESSAUTODETECT=true \
+                         -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
+                         -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
+                         -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=noops \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
+                         -e CORE_NOOPS_BLOCK_SIZE=1 \
+                         -e CORE_NOOPS_BLOCK_TIMEOUT=1s \
+                         -e CORE_SECURITY_ENABLED=false \
+                         -e CORE_LOGGING_LEVEL=${debug_level} \
+                         stylix/hyperledger-peer:noops peer node start
+     fi
+# Child node
 else
-     docker run --rm --name=${prefix}${udl}vp${node_no} \
-                    -it \
-                    --net=host \
-                    -p 5000:5000 \
-                    -p 30303:30303 \
-                    -p 30304:30304 \
-                    -p 31315:31315 \
-                    -v /var/run/docker.sock:/var/run/docker.sock \
-                    -v /hyperledger/db:/var/hyperledger/db \
-                    -v /hyperledger/production:/var/hyperledger/production \
-                    -v /hyperledger/chaincode:/var/hyperledger/chaincode \
-                    -v /hyperledger/chaincode:/go/src/chaincode \
-                    -v /hyperledger/compose:/var/hyperledger/compose \
-                    -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
-                    -e CORE_PEER_ID=vp${node_no} \
-                    -e CORE_PEER_ADDRESSAUTODETECT=true \
-                    -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
-                    -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
-                    -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=pbft \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
-                    -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
-                    -e CORE_PEER_DISCOVERY_ROOTNODE=10.32.85.82:30303\
-                    -e CORE_PBFT_GENERAL_N=4 \
-                    -e CORE_PBFT_GENERAL_K=2 \
-                    -e CORE_PBFT_GENERAL_TIMEOUT_REQUEST=10s \
-                    -e CORE_PBFT_GENERAL_MODE=classic \
-                    -e CORE_SECURITY_ENABLED=false \
-                    -e CORE_LOGGING_LEVEL=${debug_level} \
-                    stylix/hyperledger-peer:pbft peer node start
+     if [ "$consensus" == "pbft" ]
+          then
+          # Consensus PBFT mode
+          echo -e "..NOOPS.."
+          docker run --rm --name=${prefix}${udl}vp${node_no} \
+                         -it \
+                         --net=host \
+                         -p 5000:5000 \
+                         -p 30303:30303 \
+                         -p 30304:30304 \
+                         -p 31315:31315 \
+                         -v /var/run/docker.sock:/var/run/docker.sock \
+                         -v /hyperledger/db:/var/hyperledger/db \
+                         -v /hyperledger/production:/var/hyperledger/production \
+                         -v /hyperledger/chaincode:/var/hyperledger/chaincode \
+                         -v /hyperledger/chaincode:/go/src/chaincode \
+                         -v /hyperledger/compose:/var/hyperledger/compose \
+                         -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
+                         -e CORE_PEER_ID=vp${node_no} \
+                         -e CORE_PEER_ADDRESSAUTODETECT=true \
+                         -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
+                         -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
+                         -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=pbft \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
+                         -e CORE_PEER_DISCOVERY_ROOTNODE=10.32.85.82:30303\
+                         -e CORE_PBFT_GENERAL_N=4 \
+                         -e CORE_PBFT_GENERAL_K=2 \
+                         -e CORE_PBFT_GENERAL_TIMEOUT_REQUEST=10s \
+                         -e CORE_PBFT_GENERAL_MODE=classic \
+                         -e CORE_SECURITY_ENABLED=false \
+                         -e CORE_LOGGING_LEVEL=${debug_level} \
+                         stylix/hyperledger-peer:pbft peer node start
+     else
+          # Consensus NOOPS mode
+          echo -e "..NOOPS.."
+          docker run --rm --name=${prefix}${udl}vp${node_no} \
+                         -it \
+                         --net=host \
+                         -p 5000:5000 \
+                         -p 30303:30303 \
+                         -p 30304:30304 \
+                         -p 31315:31315 \
+                         -v /var/run/docker.sock:/var/run/docker.sock \
+                         -v /hyperledger/db:/var/hyperledger/db \
+                         -v /hyperledger/production:/var/hyperledger/production \
+                         -v /hyperledger/chaincode:/var/hyperledger/chaincode \
+                         -v /hyperledger/chaincode:/go/src/chaincode \
+                         -v /hyperledger/compose:/var/hyperledger/compose \
+                         -v /hyperledger/compose/net_host_.bashrc:/root/.bashrc \
+                         -e CORE_PEER_ID=vp${node_no} \
+                         -e CORE_PEER_ADDRESSAUTODETECT=true \
+                         -e CORE_PEER_SYNC_BLOCKS_CHANNELSIZE=100 \
+                         -e CORE_PEER_SYNC_STATE_SNAPSHOT_CHANNELSIZE=500 \
+                         -e CORE_PEER_SYNC_STATE_DELTAS_CHANNELSIZE=200 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_PLUGIN=noops \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_BUFFERSIZE=7500 \
+                         -e CORE_PEER_VALIDATOR_CONSENSUS_EVENTS_BUFFERSIZE=1000 \
+                         -e CORE_PEER_DISCOVERY_ROOTNODE=10.32.85.82:30303\
+                         -e CORE_NOOPS_BLOCK_SIZE=1 \
+                         -e CORE_NOOPS_BLOCK_TIMEOUT=1s \
+                         -e CORE_SECURITY_ENABLED=false \
+                         -e CORE_LOGGING_LEVEL=${debug_level} \
+                         stylix/hyperledger-peer:noops peer node start
+     fi
 fi
