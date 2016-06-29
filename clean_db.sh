3329405cc48cfb7fbf262ba051120fb1368275a7
Siriwat Kasamwattanarote <siriwat.k@sbibits.com>
2016-06-30 08:22:10 +0900

    Added docker cleaning commands

--- clean_db.sh
+++ clean_db.sh
@@ -1,2 +1,5 @@
+docker rm `docker ps --no-trunc -aq`
+docker rmi $(docker images | grep "^dev-" | awk "{print $3}")
+docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
 sudo rm -rf /hyperledger/db/*
 sudo rm -rf /hyperledger/production/*
